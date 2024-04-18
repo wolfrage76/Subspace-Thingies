7c2325cf619ca5e953eb3186686ea905f405bbf4
wolfrage76 <75458290+wolfrage76@users.noreply.github.com>
2024-04-17 23:15:19 -0400

    stuff

--- Subspace_Farm_Monitor_Thingy/viewer/utilities/conf.py
+++ Subspace_Farm_Monitor_Thingy/viewer/utilities/conf.py
@@ -1,3 +1,254 @@
+from collections import defaultdict
+
+# Initialize state
+allSummedPlotting = {}
+allSummedplottingCount = {}
+avgtime = defaultdict(lambda: {})
+balance = "0.0 tSSC"
+banners = 'Visit http://subspace.ifhya.com for community tools!'
+best_block = 0
+cache_size = 1
+current_farmer_index = 0
+curr_farm_name = "Default"
+curr_farm = None
+curr_line = str()
+curr_sector_disk = defaultdict(lambda: 0)
+deltas = defaultdict(lambda: '00:00')
+disk_farms = set()
+dl = 0
+drive_directory = {}
+errors = [str(), str(), str(), str(), str(), str(), str(),]
+event_times = defaultdict(int)
+farm_deltas = defaultdict(lambda: {})
+farmer_data = {}
+farmer_name = 'WolfrageRocks'  # str()
+farm_metrics = {}
+farm_names = []
+farm_plot_size = defaultdict(lambda: "0")
+farm_recent_rewards = defaultdict(lambda: {})
+farm_recent_skips = defaultdict(int)
+farm_rewards = defaultdict(lambda: {})
+farm_skips = defaultdict(int)
+finalized = 0
+frame_delays = -1
+hour_24 = False
+imported = 0
+is_syncing = True
+lang = {}
+last_farm = int()
+last_logs = [str(), str(), str(), str(), str(), str(), str(),]
+# However many initialized is how many it'll show
+last_node_logs = [str(), str(), str(), str(), str(), str(),]
+last_sector_time = defaultdict(int)
+last_sector_only = True
+latest_version = "Unknown"
+layout = None
+name = str()
+node_errors = [str(), str(), str(), str(), str(), str(), str(),]
+#node_log_file = str()
+node_warnings = [str(), str(), str(), str(), str(), str(), str(),]
+no_more_drives = False
+paused = False
+peers = 0
+plot_space = {}
+plot_time_seconds = 0.0
+prove_method = {}
+psdTotal = 0
+psTotal = 0
+remote_farms = {}
+remTotal = 0
+replotting = defaultdict(lambda: False)
+reward_count = 0
+rewards_per_hr = defaultdict(lambda: [])
+running = False
+secTime = {}
+sector_times = defaultdict(lambda: 0)
+show_logging = True
+startTime = 0
+sum_plotted = defaultdict(lambda: {})
+sum_size = defaultdict(lambda: {})
+system_stats = {}
+toggle_encoding = False
+toggle_encoding_node = False
+total_completed = 0
+total_error_count = 0
+translation = {}
+ui_port = '8016'
+ul = 0
+unroll = False
+ver = ''
+view_state = 1
+view_xtras = True
+wallet = str()
+warning_farms = []
+warnings = [str(), str(), str(), str(), str(), str(), str(),]
+theme_data = None
+theme = None
+
+translation['en'] = {
+    'farm': 'Farm',
+    'farmers': 'Farmers',
+    'ram': 'RAM',
+    'uptime': 'Up', 
+    'day': 'day',
+    'load': 'Load',
+    'rewards': 'Rewards',
+    'global_stats': 'Global Stats',
+    'single_hits': 'H',
+    'single_misses': 'M',
+    'avgsector': 'Avg Sector',
+    'lastsector':'Last Sector',
+    'eta': 'ETA',
+    'hour': 'hr',
+    'plots': 'Plots',
+    'block': 'Block',
+    'peers': 'Peers',
+    'synced': 'Synced',
+    'unsynced': 'Unsynced',
+    'latest': 'Latest',
+    'replotting': 'Replotting',
+    'report': 'Report',
+    'inactivity_removal': 'Removed due to inactivity',
+    'spacebar': 'SPACE',
+    'tab': 'TAB',
+    'pause': 'Pause',
+    'toggle_data': 'Toggle Data',
+    'change_display': 'Change Display',
+    'quit': 'uit', # Done for English as Q is the displayed menu key. Use your full word for Quit
+    'hm': 'H/M',
+    'defaultbanner': 'Check out the community tools at:', 
+    'an_error_occured':'an error occured' , 
+    'pausing':'pausing' , 
+    'exiting_requested':'Exiting as requested...' , 
+    'error_sending':'Error sending to ##' , # wherever '##' is in the text will be replaced automatically    'wallet_error':'Wallet error',
+    'decode_error': 'Decode error encountered (Toggle TOGGLE_ENCODING in the config.yaml file!)',
+    'connection_closed': 'Connection Closed',
+    'retrying_seconds': 'Retrying in ## seconds', # wherever '##' is in the text will be replaced automatically
+    'cycle_theme': 'Cycle Theme',
+}
+
+
+translation['cn'] = {
+                'farmer': '农场',
+                'farmers': '农民',
+                'ram': '内存',
+                'uptime':'运行时间',
+                'day': '天',
+                'load': '加载', 
+                'rewards': '奖励',
+                'global_stats': '全球统计',
+                'single_hits': '赢',
+                'single_misses': '丢失', 
+                'avgsector':'平均扇区时间', 
+                'eta':'埃塔',
+                'hour': '小时',
+                'plots': '地块', 
+                'block': '当前区块高度',
+                'peers':'Anode同行数量', 
+                'synced':'已同步', 
+                'unsynced': '不同步',
+                'latest': '最新版本', 
+                'replotting': '重新绘制', 
+                'report': '报告', 
+                'inactivity_removal': '因不活跃而被删除',
+                'spacebar': '空格键',
+                'tab': 'TAB键',
+                'pause':'暂停', 
+                'toggle_data': '切换数据', 
+                'change_display':'改变显示',
+                'quit':'退出', 
+                'hm': '[green]赢[yellow]/[red]错过', 
+                'defaultbanner': '查看社区工具：',
+                'cpu': 'CPU',
+                'an_error_occured':'发生了错误' , 
+                'pausing':'暂停' , 
+                'exiting_requested':'根据请求退出...' , 
+                'error_sending':'发送到 ## 时出错' ,
+                'wallet_error':'钱包错误',
+                'decode_error': '遇到解码错误（在config.yaml文件中切换TOGGLE_ENCODING!）',
+                'connection_closed': '连接关闭',
+                'retrying_seconds': '暂停 ## 秒',
+                'lastsector':'最后一个扇区',
+                'cycle_theme': '循环主题' 
+                }
+
+
+translation['lol'] = {
+    'farm': 'Fam',
+    'farmers': 'Fam',
+    'ram': 'RAM',
+    'uptime': "Chillin'",
+    'day': 'day',
+    'load': 'Load',
+    'rewards': 'Riz',
+    'global_stats': 'Spilling Tea',
+    'single_hits': ':thumbs_up:',
+    'single_misses': ':pile_of_poo:',
+    'avgsector': 'Work Shift',
+    'eta': "Quittin' Time",
+    'hour': 'hr',
+    'plots': 'Plots',
+    'block': 'Block',
+    'peers': 'Peeps',
+    'synced': 'In the Know',
+    'unsynced': 'Confused',
+    'latest': 'Latest',
+    'replotting': 'Do Over',
+    'report': 'Report',
+    'inactivity_removal': 'Removed due to inactivity',
+    'spacebar': 'SPACE',
+    'tab': 'TAB',
+    'pause': 'Pause',
+    'toggle_data': 'Toggle Data',
+    'change_display': 'Change Display',
+    'quit': 'uit',
+    'hm': 'H/M',
+    'defaultbanner': 'Check out the community tools at:', 
+    'an_error_occured':'an error occured' , 
+    'pausing':'pausing' , 
+    'exiting_requested':'Exiting as requested...' , 
+    'error_sending':'Error sending to ' ,     'wallet_error':'Wallet error',
+    'decode_error': 'Decode error encountered (Toggle TOGGLE_ENCODING in the config.yaml file!)',
+    'connection_closed': 'Connection Closed',
+    'retrying_seconds': 'Retrying in ## seconds'
+     
+}
+
+
+translation['ru'] = {
+    'farm': 'Ферма',
+    'farmers': 'Фермеры',
+    'ram': 'ОЗУ',
+    'uptime': 'Время работы',
+    'day': 'день',
+    'load': 'Нагрузка',
+    'rewards': 'Награды',
+    'global_stats': 'Глобальная статистика',
+    'single_hits': 'H',
+    'single_misses': 'М',
+    'avgsector': 'Среднее Сектор',
+    'eta': 'ОЖ',
+    'hour': 'час',
+    'plots': 'Плоты',
+    'block': 'Блок',
+    'peers': 'Пиры',
+    'synced': 'Синхронизировано',
+    'unsynced': 'Не синхронизировано',
+    'latest': 'Последняя версия',
+    'replotting': 'Перепланировка',
+    'report': 'Отчет',
+    'inactivity_removal': 'Удалено из-за неактивности',
+    'spacebar': 'ПРОБЕЛ',
+    'tab': 'ТАБ',
+    'pause': 'Пауза',
+    'toggle_data': 'Переключить данные',
+    'change_display': 'Изменить отображение',
+    'quit': 'Выход',
+    'hm': '[green]H[yellow]/[red]М',
+    'defaultbanner': 'Ознакомьтесь с инструментами сообщества на:',
+                'lastsector':'Last Sector',
+                'cycle_theme': 'Cycle Theme' 
+}
 
 from collections import defaultdict
 
